498ba6b25269098ebbec99cc2cbdfab3c4e7620a
Michael Park <michael511.mp@gmail.com>
2017-11-18 15:58:32 -0500

    packagae update

--- modules/watsonvr/watsonvr.js
+++ modules/watsonvr/watsonvr.js
@@ -1,7 +1,7 @@
 var watson = require('watson-developer-cloud');
 var fs = require('fs');
 var visual_recognition = watson.visual_recognition({
-    api_key: '{0121N-TKK90-18D9D-0A2A0-3DVH1}',
+    api_key: '0121N-TKK90-18D9D-0A2A0-3DVH1',
     version: 'v3',
     version_date: '2016-05-20'
 });
@@ -17,3 +17,17 @@ visual_recognition.detectFaces(params,
         else
             console.log(JSON.stringify(response, null, 2));
     });
+
+Module.register("watsonvr",{
+    // Default module config.
+    defaults: {
+        text: "Hello World!"
+    },
+
+    // Override dom generator.
+    getDom: function() {
+        var wrapper = document.createElement("div");
+        wrapper.innerHTML = this.config.text;
+        return wrapper;
+    }
+});
